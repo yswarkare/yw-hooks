4914917449bc462c519dfb89835c9b2618f4974d
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-11-16 22:31:30 +0530

    updating main branch

--- lib/hooks/themes/index.ts
+++
@@ -1,9 +0,0 @@
-import darkTheme from "./darkTheme"
-import defaultTheme from "./defaultTheme"
-import lightTheme from "./lightTheme"
-
-export {
-  darkTheme,
-  defaultTheme,
-  lightTheme,
-}
--- lib/index.ts
+++
@@ -1,6 +0,0 @@
-import useLocalStorage from './hooks/useLocalStorage';
-import useSessionReducer from './hooks/useSessionReducer';
-import useSessionStorage from './hooks/useSessionStorage';
-import { useContextState, ContextStateProvider, useUpdateContextState } from './hooks/ContextState';
-
-export { useLocalStorage, useSessionReducer, useSessionStorage, useContextState, ContextStateProvider, useUpdateContextState };
